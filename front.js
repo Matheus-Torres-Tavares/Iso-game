f7ba174d735d9a644a37c58da6f117ac607e76d6
matheus <mtava002@gmail.com>
2020-09-08 16:08:25 -0400

    update

--- front.js
+++ front.js
@@ -5,30 +5,21 @@ var scaling = "fit";
 var width = 1280;
 var height = 960;
 var hero = ["img/arn1.png"]
-
 var frame = new Frame(scaling, width, height);
 frame.on("ready", function () {
     zog("ready from ZIM Frame");
-
     var stage = frame.stage;
     var stageW = frame.width;
     var stageH = frame.height;
     frame.outerColor = "#444";
     frame.color = "#ddd";
-
-    // new Sprite(hero)
-    //     .center()
-    //     .run({ time: 1000, loop: true });
-
-
     var holder = new Container();
-
     var tiles = new Tile(new Rectangle(70, 70, frame.light, frame.dark).centerReg(), 6, 6)
         .rot(45)
         .addTo(holder)
         .outline()
     holder.sca(2, 1).center();
-
+    window.tiles = tiles
     tiles.on("mouseover", function (e) {
         e.target.color = frame.grey;
         stage.update();
@@ -37,40 +28,24 @@ frame.on("ready", function () {
         e.target.color = frame.light;
         stage.update();
     })
-
-    // var ball = new Circle(40, frame.blue, frame.dark).center().sha();
-
-    // tiles.on("click", function (e) {
-    //     console.log('check check')
-    //     var point = tiles.localToGlobal(e.target.x, e.target.y)
-    //     token2.animate({
-    //         obj: {
-    //             x: point.x,
-    //             y: point.y
-    //         },
-    //         time: 1,
-    //         events: true
-    //     });
-    //     stage.update();
-    // })
-    //     ;
-
-
-
     class Token extends Circle {
         constructor(size, color, border, attack, health, team) {
             super(size, color, border)
             this.attack = attack
             this.health = health
             this.team = team
-
+            this.active = false
         }
         whoAmI() {
             console.log(this.team)
+            for (let token of tokenArray) {
+                token.active = false
+            }
+            this.active = true//!this.active
         }
         moveToken(e) {
-
             var point = tiles.localToGlobal(e.target.x, e.target.y)
+            console.log(point)
             this.animate({
                 obj: {
                     x: point.x,
@@ -79,81 +54,50 @@ frame.on("ready", function () {
                 time: 1,
                 events: true
             });
+            setTimeout(function () {
+                console.log('next')
+            }, 1000)
             stage.update();
         }
     }
-
-
-    let token = new Token(30, frame.red, frame.dark, 15, 30, "red").center();
-    let token2 = new Token(70, frame.blue, frame.dark, 15, 30, "blue").center();
-    let tokenArray = [token, token2]
-    tokenArray.map(v => {
-
-    })
-    window.token = token
-    token.on("click", function (e) {
-        this.whoAmI()
-
-        tiles.on("click", function (e) {
-            console.log('check check')
-            var point = tiles.localToGlobal(e.target.x, e.target.y)
-            token.animate({
-                obj: {
-                    x: point.x,
-                    y: point.y
-                },
-                time: 1,
-                events: true
-            });
-            stage.update();
-            tiles.unbind("click")
-        });
-
-
-    })
-
-    token2.on("click", function (e) {
-        this.whoAmI()
-        tiles.on("click", function (e) {
-            console.log('check check')
-            var point = tiles.localToGlobal(e.target.x, e.target.y)
-            token2.animate({
-                obj: {
-                    x: point.x,
-                    y: point.y
-                },
-                time: 1,
-                events: true
-            });
-            stage.update();
-            tiles.off("click")
-        });
-
-
-    })
+    let token = new Token(40, frame.red, frame.dark, 15, 30, "red").center();
+    let token3 = new Token(35, frame.purple, frame.dark, 15, 30, "purple").center();
+    let token4 = new Token(30, frame.green, frame.dark, 15, 30, "green").center();
+    let token2 = new Token(25, frame.blue, frame.dark, 15, 30, "blue").center();
     var proportion = new Proportion(0, stageH, .8, 1.3);
-    token2.on("animation", function () {
-        token2.sca(proportion.convert(token2.y));
-    });
-
+    let tokenArray = [token, token2, token3, token4]
+    // lets add our events in this loop
+    for (let token of tokenArray) {
+        // create a click event on token to set it to become active
+        token.on("click", token.whoAmI)
+        token.on("animation", function () {
+            console.log('blah')
+            token.sca(proportion.convert(token.y));
+        })
+    }
+    window.token = token
+    tiles.on("click", function (e) {
+        console.log('check check')
+        for (let token of tokenArray) {
+            // if token has been confirmed to be active, it can be moved
+            if (token.active) {
+                token.moveToken(e)
+            }
+            // resetting tokens activity to false after each movement
+            token.active = false
+        }
+    })
+    function collisionDetection() {
+        for (let token of tokenArray) {
+            for (let tile of tiles.children) {
+                console.log(token, tile)
+                tile.color = frame.red
+            }
+        }
+    }
+    window.collisionDetection = collisionDetection
+    // token2.on("animation", function () {
+    //     token2.sca(proportion.convert(token2.y));
+    // });
     stage.update();
-
 });
-
-
-
-// class Char {
-//     constructor(img,spriteW, spriteH, rows, cols, width, height,x,y,srcX, srcY) {
-//         this.img = img;
-//         this.spriteW = spriteW;
-//         this.spriteH = spriteH,
-//         this.rows = rows;
-//         this.cols = cols;
-//         this.width = width;
-//         this.height = height;
-//         this.x = x;
-//         this.y = y;
-//         this.srcX = srcX;
-//         this.srcY = srcY;
-//     }
-// }
